b117eb4e6fcb0c4644627a7e494eb3edb94cb2b1
ansusharma <kranshu6208@gmail.com>
2021-07-16 19:55:24 +0530

    add increase quantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,45 +1,61 @@
 import React from 'react';
 class CartItem extends React.Component {
-    constructor () {
+    constructor() {
         super();
         this.state = {
             price: 500,
             title: 'Mobile Phone',
             quantity: 2,
             img: ''
-            
+
         }
+    }
+    increaseQuantity=()=>{
+        console.log('this',this.state);
     }
 
 
     render() {
-        const {price,title,quantity}=this.state
+        const { price, title, quantity } = this.state
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} />
 
                 </div>
-                    <div className="right-block">
-                        <div style={{fontSize: 35}}>{title}</div>
-                        <div style={{color: '#a81e4a'}}>Rs {price}</div>
-                        <div style={{color: '#1e7fa8'}}> Quantity: {quantity}</div>
-                        <div className="cart-item-actions">
-                            {/* buttons */}
-                            <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/png/128/1828/1828926.png"/>
-                            <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/png/128/992/992683.png"/>
-                            <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/png/128/1214/1214428.png"/>
-
-                        </div>
-
+                <div className="right-block">
+                    <div style={{ fontSize: 35 }}>{title}</div>
+                    <div style={{ color: '#a81e4a' }}>Rs {price}</div>
+                    <div style={{ color: '#1e7fa8' }}> Quantity: {quantity}</div>
+                    <div className="cart-item-actions">
+                        {/* buttons */}
+                        <img
+                            alt="increase"
+                            className="action-icons"
+                            src="https://image.flaticon.com/icons/png/128/1828/1828926.png"
+                            onClick={this.increaseQuantity.bind(this)}
+                        />
+                        <img
+                            alt="decrease"
+                            className="action-icons"
+                            src="https://image.flaticon.com/icons/png/128/992/992683.png"
+                        />
+                        <img
+                            alt="delete"
+                            className="action-icons"
+                            src="https://image.flaticon.com/icons/png/128/1214/1214428.png"
+                        />
 
                     </div>
 
+
                 </div>
-                );
+
+            </div>
+        );
 
 
-        
+
     }
 }
 const styles = {
